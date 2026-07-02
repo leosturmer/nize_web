aa01d4c5bee6710116b700905ae9b749f6a8424a
leosturmer <leo@email.com>
2026-07-02 11:44:47 -0300

    botão acessibilidade surdos

--- php/view/gui_alteracao_produto.php
+++ php/view/gui_alteracao_produto.php
@@ -179,16 +179,6 @@ $produto = $produtoDAO->buscarPorId($id_produto);
         }
     </script>
 
-    <div vw class="enabled">
-        <div vw-access-button class="active"></div>
-        <div vw-plugin-wrapper>
-            <div class="vw-plugin-top-wrapper"></div>
-        </div>
-    </div>
-    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
-    <script>
-        new window.VLibras.Widget('https://vlibras.gov.br/app');
-    </script>
 </body>
 
 
