33f9faad1b40d0d803fd43407a520e120cdb47af
leosturmer <leo@leo.com>
2026-07-11 19:01:11 -0300

    tentando aí

--- php/view/gui_visualizacao_produtos.php
+++ php/view/gui_visualizacao_produtos.php
@@ -109,7 +109,7 @@ $lista = $produtoDAO->listarTodosProdutos($usuario->id_usuario);
 
       <div class="internal-nav-links">
         <h1>Lista de Produtos</h1>
-        <a href="gui_cadastro_produtos.php">Cadastrar novo produto</a>
+        <a href="gui_cadastro_produtos.php"><span class="bi bi-plus"></span>Produto</a>
       </div>
 
       <div class="internal-nav-inputs">
